11efc77c168bbe89ed50fa4906e049dc70a148db
Martinho Macedo <martinhomacedo111@gmail.com>
2024-07-31 18:35:27 +0100

    exercise 1.14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -23,18 +23,36 @@ const App = () => {
 
   const randomAnecdote = () => {setSelected(getRandomInt(anecdotes.length))}
 
+  const mostVotedIdx= () => {
+    let idx = 0
+    for (let i=0; i<anecdotes.length; i++){
+      if(points[i] > points[idx]){
+        idx = i
+      }
+    }
+    return idx
+
+  }
+
   const vote = () => {
     const copy = [...points]
     copy[selected] += 1
     setPoints(copy)
   }
 
+
+  let mvIdx = mostVotedIdx()
+
   return (
     <div>
+      <h1> Anecdote of the day </h1>
       <Display text={anecdotes[selected]}/>
       <Display text={"has " + points[selected] + " votes"}/>
       <Button handleClick={vote} text="vote"/>
       <Button handleClick={randomAnecdote} text="next anecdote"/>
+      <h1> Anecdote with most votes</h1>
+      <Display text={anecdotes[mvIdx]}/>
+      <Display text={"has " + points[mvIdx] + " votes"}/>
     </div>
   )
 }
